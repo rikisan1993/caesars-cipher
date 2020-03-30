face7755605459f1f1e55e9693e720178faf2d6d
rikisan1993 <kosiriki@gmail.com>
2020-03-30 22:36:16 +0700

    Initial Commit

---
+++ src/test/java/caesars/cipher/CaesarsCipherTest.java
@@ -0,0 +1,21 @@
+package caesars.cipher;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CaesarsCipherTest {
+	
+	private CaesarsCipher caesarsCipher = new CaesarsCipher();
+	
+	@Test
+	public void testCipheredMessageWithOffsetOf12() {
+		assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+	}
+	
+	@Test
+	public void testEmptyString() {
+		assertEquals("", caesarsCipher.cipher("", 12));
+	}
+
+}
